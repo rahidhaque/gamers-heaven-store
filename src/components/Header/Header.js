97e3130b45ba406ed5484729fdf4e407b5330ca5
rahidhaque <ontirahid@yahoo.com>
2022-03-25 20:46:25 +0600

    Header Added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='header'>
+                <h1>Gamers Heaven Dokan</h1>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
